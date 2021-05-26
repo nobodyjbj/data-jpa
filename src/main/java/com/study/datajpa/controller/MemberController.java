e04bbe86ece6661e73b768d2d6a4a3309b90f937
jbj <nobody.jbj@gmail.com>
2021-05-26 17:30:36 +0900

    paging and sort

--- src/main/java/com/study/datajpa/controller/MemberController.java
+++ src/main/java/com/study/datajpa/controller/MemberController.java
@@ -1,8 +1,12 @@
 package com.study.datajpa.controller;
 
+import com.study.datajpa.dto.MemberDto;
 import com.study.datajpa.entity.Member;
 import com.study.datajpa.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,8 +30,15 @@ public class MemberController {
         return member.getUsername();
     }
     
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5) Pageable pageable) {
+        return memberRepository.findAll(pageable).map(MemberDto::new);
+    }
+    
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("userA"));
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
     }
 }
--- src/main/java/com/study/datajpa/dto/MemberDto.java
+++ src/main/java/com/study/datajpa/dto/MemberDto.java
@@ -1,5 +1,6 @@
 package com.study.datajpa.dto;
 
+import com.study.datajpa.entity.Member;
 import lombok.*;
 
 @Getter @Setter @AllArgsConstructor @NoArgsConstructor @ToString
@@ -8,5 +9,10 @@ public class MemberDto {
     private Long id;
     private String username;
     private String teamName;
+    
+    public MemberDto(Member member) {
+        this.id = member.getId();
+        this.username = member.getUsername();
+    }
 
 }
