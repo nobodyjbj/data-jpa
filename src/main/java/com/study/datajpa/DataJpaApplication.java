cbb110d49a244410115c77170a6494355fb73f17
jbj <nobody.jbj@gmail.com>
2021-05-26 16:57:45 +0900

    auditing

--- src/main/java/com/study/datajpa/DataJpaApplication.java
+++ src/main/java/com/study/datajpa/DataJpaApplication.java
@@ -2,12 +2,25 @@ package com.study.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class DataJpaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DataJpaApplication.class, args);
+    }
+    
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        // 실제로는 SecurityContext의 유저정보를 꺼내서 넣어주면 된다.
+        return () -> Optional.of(UUID.randomUUID().toString());
     }
 
 }
--- src/test/java/com/study/datajpa/entity/MemberTest.java
+++ src/test/java/com/study/datajpa/entity/MemberTest.java
@@ -1,6 +1,8 @@
 package com.study.datajpa.entity;
 
+import com.study.datajpa.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,6 +18,9 @@ class MemberTest {
 
     @PersistenceContext
     EntityManager em;
+    
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     public void testEntity() {
@@ -43,5 +48,27 @@ class MemberTest {
             System.out.println("member :: " + member);
             System.out.println(" --> member.team :: " + member.getTeam());
         }
+    }
+    
+    @Test
+    public void JpaEventBaseEntity() throws Exception {
+        // given
+        Member member = new Member("member1");
+        memberRepository.save(member);
+        
+        Thread.sleep(100);
+        member.setUsername("member2");
+        
+        em.flush();
+        em.clear();
+        
+        // when
+        Member findMember = memberRepository.findById(member.getId()).get();
+    
+        // then
+        System.out.println("getCreateDate : " + findMember.getCratedDate());
+        System.out.println("getUpdateDate : " + findMember.getLastModifiedDate());
+        System.out.println("getCreatedBy : " + findMember.getCreatedBy());
+        System.out.println("getLastModifiedBy : " + findMember.getLastModifiedBy());
     }
 }
