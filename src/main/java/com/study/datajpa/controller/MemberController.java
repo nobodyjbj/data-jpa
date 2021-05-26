f45041bc86d57e9b6e0daa48ed1151fa21a8cb7e
jbj <nobody.jbj@gmail.com>
2021-05-26 18:43:44 +0900

    새로운 엔티티 구별 방법

--- src/main/java/com/study/datajpa/controller/MemberController.java
+++ src/main/java/com/study/datajpa/controller/MemberController.java
@@ -35,10 +35,12 @@ public class MemberController {
         return memberRepository.findAll(pageable).map(MemberDto::new);
     }
     
+    /*
     @PostConstruct
     public void init() {
         for (int i = 0; i < 100; i++) {
             memberRepository.save(new Member("user" + i, i));
         }
     }
+    */
 }
